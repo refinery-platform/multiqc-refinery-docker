b211bc736544038b7674bc5efd994bfadaad2e49
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-05-04 11:30:48 -0400

    Factor out the test runner

--- test.sh
+++ test.sh
@@ -6,36 +6,35 @@ start() { echo travis_fold':'start:$1; echo $1; set -v; }
 end() { set +v; echo travis_fold':'end:$1; echo; echo; }
 cleanup() { docker stop $CONTAINER_NAME; docker rm $CONTAINER_NAME; }
 
+test() {
+    FIXTURE=$1
+    shift
+    echo "Run with $FIXTURE"
+    ./run.sh $FIXTURE
+    OUTPUT=/tmp/multiqc.html
+    curl http://localhost:$PORT/ > $OUTPUT
+    while (( "$#" )); do
+        PATTERN=$1
+        shift
+        echo "Looking for '$PATTERN'..."
+        grep --only-matching "$PATTERN" "$OUTPUT" \
+            || die "Didn't find '$PATTERN' in '$OUTPUT'"
+    done
+    echo 'Cleaning up...'
+    cleanup
+}
+
 source shared.sh
 
 
-start docker_build
+start build
 ./build.sh
-end docker_build
-
-
-OUTPUT=/tmp/multiqc.html
-
+end build
 
 start test_good
-./run.sh fixtures/good-input.json
-curl http://localhost:$PORT/ > $OUTPUT
-for TOOL in 'General Stats' 'Bowtie 2' 'FastQC'; do
-    echo "Looking for '$TOOL'..."
-    grep --only-matching "$TOOL" "$OUTPUT" \
-    || die "Didn't find '$TOOL' in '$OUTPUT'"
-done
-cleanup
+test fixtures/good-input.json 'General Stats' 'Bowtie 2' 'FastQC'
 end test_good
 
-
 start test_empty
-./run.sh fixtures/empty-input.json
-curl http://localhost:$PORT/ > $OUTPUT
-for TOOL in 'MultiQC did not run'; do
-    echo "Looking for '$TOOL'..."
-    grep --only-matching "$TOOL" "$OUTPUT" \
-    || die "Didn't find '$TOOL' in '$OUTPUT'"
-done
-cleanup
+test fixtures/empty-input.json 'MultiQC did not run'
 end test_empty
