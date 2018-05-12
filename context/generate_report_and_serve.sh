c9eea64b4e50cb1e96efaba61f978dacbd7a7307
Scott Ouellette <scottx611x@gmail.com>
2018-05-12 06:33:50 -0400

    Add INPUT_JSON_URL support

--- context/generate_report_and_serve.sh
+++ context/generate_report_and_serve.sh
@@ -13,6 +13,16 @@ import json
 import wget
 
 input_json = json.loads(os.environ['INPUT_JSON'])
+
+input_json = None
+
+if 'INPUT_JSON' in os.environ:
+    input_json = json.loads(os.environ["INPUT_JSON"])
+elif 'INPUT_JSON_URL' in os.environ:
+    input_json = requests.get(os.environ["INPUT_JSON_URL"]).json()
+else:
+    raise Exception('Did not find expected environment variable')
+    
 for url in input_json['file_relationships']:
     wget.download(url)
 EOF
