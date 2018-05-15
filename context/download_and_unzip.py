9f98a2d0d5c8f330d6b4cded0df7d4c963797ce7
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-05-15 11:55:32 -0400

    Checkpoint download and unzip

--- context/download_and_unzip.py
+++ context/download_and_unzip.py
@@ -3,18 +3,29 @@ import json
 import wget
 import zipfile
 
-def unzip_all_in_dir(parent_dir):
-    # Zip files with absolute paths could clobber other directories:
+def unzip_all_in_dir(top):
+    '''
+    >>> fixtures = os.path.dirname(os.path.realpath(__file__)) + '/../fixtures'
+    >>> d = fixtures + '/d'
+    >>> assert os.listdir(d) == ['c.zip']
+    >>> unzip_all_in_dir(fixtures + '/d')
+    >>> os.listdir(d)
+    
+    '''
+    # Note that zip files with absolute paths could clobber other directories:
     # https://docs.python.org/3/library/zipfile.html#zipfile.ZipFile.extractall
-    for path in (os.path.join(parent_dir, f) for f in os.listdir(parent_dir)):
-        print('{} is zip?'.format(path))
-        if zipfile.is_zipfile(path):
-            child_dir = path + '.unzipped'
-            os.mkdir(child_dir)
-            print('{} -> {}'.format(path, child_dir))
-            with zipfile.ZipFile(path) as z:
-                z.extractall(child_dir)  # Extract *to* child_dir
-                unzip_all_in_dir(child_dir)
+    for root, dirs, files in os.walk(top):
+        for dir in dirs:
+            abs_dir = os.path.join(top, dir)
+            unzip_all_in_dir(abs_dir)
+        for file in files:
+            abs_file = os.path.join(top, file)
+            if zipfile.is_zipfile(abs_file):
+                new_dir = abs_file + '.unzipped'
+                os.mkdir(new_dir)
+                with zipfile.ZipFile(abs_file) as z:
+                    z.extractall(new_dir)  # Extract *to* new_dir
+                    unzip_all_in_dir(new_dir)
 
 if __name__ == '__main__':
     if 'INPUT_JSON_URL' in os.environ:
